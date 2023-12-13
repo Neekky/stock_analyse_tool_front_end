f656bd53d2fd1cc22192cc18e058d3d4dcc69a7a
cengchao <475383200@qq.com>
2023-12-13 23:56:21 +0800

    feat: 开发部署

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,37 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, './src') // 路径别名
+export default (props: any) => {
+  const { mode } = props;
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, './src') // 路径别名
+      },
     },
-  },
-})
+    server: {
+      host: "127.0.0.1",
+      port: Number(loadEnv(mode, process.cwd()).VITE_APP_PORT),
+      // https: true,
+      // strictPort: true, // 端口被占用直接退出
+      open: true, // 在开发服务器启动时自动在浏览器中打开应用程序
+      proxy: {
+        // 字符串简写写法
+        "^/api": {
+          target:
+            mode === "development"
+              ? loadEnv(mode, process.cwd()).VITE_APP_DEV_URL
+              : loadEnv(mode, process.cwd()).VITE_APP_PROD_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+      hmr: {
+        overlay: false, // 屏蔽服务器报错
+      },
+    },
+  })
+}
