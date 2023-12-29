71c8fb8d8fd8a011218dff3789cb3c9df081f87d
ZengChao <42760076+Neekky@users.noreply.github.com>
2023-12-29 17:48:00 +0800

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as Axios } from "./request"; 
 export { default as Amount } from "./amount"; 
-export { default as Calculate } from "./calculate"; 
+export { default as Calculate } from "./calculate";
+export { default as dataConversion } from "./data-conversion";
