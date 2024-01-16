423fb09b5dd548db9be34a98a765f930af56cc79
cengchao <475383200@qq.com>
2024-01-16 23:37:05 +0800

    feat: 增加个股查询能力升级

--- src/pages/leading-trend-analysis/components/hot-plate-rank/index.tsx
+++ src/pages/leading-trend-analysis/components/hot-plate-rank/index.tsx
@@ -1,23 +1,42 @@
-import { allInfoApi } from '@/apis';
-import React, { useEffect } from 'react';
+import { allInfoApi } from "@/apis";
+import React, { useEffect, useState } from "react";
+import "./index.less";
 
 const HotPlateRank: React.FC = () => {
+  const [plateData, setPlateData] = useState([]);
 
-    useEffect(() => {
-        get_hot_plate_data('20240115')
-    }, []);
+  useEffect(() => {
+    get_hot_plate_data("20240116");
+  }, []);
 
-    const get_hot_plate_data = async (date: string) => {
-        const res = await allInfoApi.get_hot_plate_data(date);
-
-        console.log(res);
+  const get_hot_plate_data = async (date: string) => {
+    const res = await allInfoApi.get_hot_plate_data(date);
+    if (res.code === 200) {
+      const data = JSON.parse(res.data);
+      setPlateData(data);
+      console.log(data);
     }
+  };
 
-    return (
-        <div>
-            {/* Render your component content here */}
-        </div>
-    );
+  return (
+    <div className="hot-plate-wrap">
+      <div className="title">今日热点板块</div>
+      <div className="plate-list-wrap">
+        {plateData.map((ele: any) => {
+          return (
+            <div className="plate-item" key={ele.code}>
+              <div className="rank-tag">{ele?.order}</div>
+              <div className="item-name">{ele?.name}</div>
+              {ele?.hot_tag && (
+                <div className="hot-tag-wrap">{ele?.hot_tag}</div>
+              )}
+              {ele?.tag && <div className="tag-wrap">{ele?.tag}</div>}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default HotPlateRank;
