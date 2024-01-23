ba77c56cd0144a9737ebb65538a333c12fb3fe94
cengchao <475383200@qq.com>
2024-01-23 22:41:40 +0800

    feat: 增加寻龙板块功能升级

--- src/pages/leading-trend-analysis/index.tsx
+++ src/pages/leading-trend-analysis/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-// import { message } from "antd";
+import { Spin } from "antd";
 import HotPlateRank from "./components/hot-plate-rank";
 import HotPlateStockList from "./components/hot_plate_stock_list";
 import "./index.less";
@@ -10,35 +10,45 @@ export default function Index(): any {
 
   const [allStockData, setAllStockData] = useState<any[]>([]);
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     getAllStockData();
   }, []);
 
   const getAllStockData = async () => {
     const res = await allInfoApi.get_all_stock_data();
-    console.log(res, 'allStockData')
+    console.log(res, "allStockData");
     if (res.code === 200) {
       const data = JSON.parse(res.data);
       setAllStockData(data);
       setPid(data[0].code);
     }
-  }
-  
+    setLoading(false);
+  };
 
   return (
     <div className="leading-trend-wrapper">
       {/* {contextHolder} */}
       {/* 上层 */}
-      <div className="trend-top">
-        {/* 今日热点板块 */}
-        <HotPlateRank setPid={setPid} />
+      {loading ? (
+        <div className="loading-wrap">
+          <Spin size="large" />
+        </div>
+      ) : (
+        <>
+          <div className="trend-top">
+            {/* 今日热点板块 */}
+            <HotPlateRank setPid={setPid} />
 
-        {/* 热点板块龙头股，夹带自我排名 */}
-        <HotPlateStockList allStockData={allStockData} id={pid} />
-      </div>
+            {/* 热点板块龙头股，夹带自我排名 */}
+            <HotPlateStockList allStockData={allStockData} id={pid} />
+          </div>
 
-      {/* 中层 */}
-      <div>{/* 股票走势 */}</div>
+          {/* 中层 */}
+          <div>{/* 股票走势 */}</div>
+        </>
+      )}
     </div>
   );
 }
