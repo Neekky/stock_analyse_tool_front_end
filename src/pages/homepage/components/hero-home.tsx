d1250347af61f06aed2f6db9f563ccfdfe470351
cengchao <475383200@qq.com>
2024-08-07 23:51:39 +0800

    1.功能开发

--- src/pages/homepage/components/hero-home.tsx
+++ src/pages/homepage/components/hero-home.tsx
@@ -1,6 +1,8 @@
 import PageIllustration from "./page-illustration";
 import { useEffect, useState } from "react";
 import { allInfoApi } from "@/apis";
+import "./hero-home.less";
+
 export default function HeroHome() {
   const [indexData, setIndexData] = useState({
     consecutive_up_days: 0,
@@ -19,8 +21,56 @@ export default function HeroHome() {
     }
   };
 
+  const renderTrendWord = (percent, score) => {
+    const numPer = Number(percent);
+    let word: any = null;
+    if (numPer >= 80) {
+      word =
+        score > 0 ? (
+          <div>
+            当前正在接近趋势顶部，开始注意风险，密切关注成交量，发现当日成交量减少后，可开始逐渐减仓。
+            <br />
+            同时结合趋势分析结果，综合判断，当从上升趋势转为下跌趋势时，轻仓操作。
+            <br />
+            见顶过程中，一般会出现天量。或是在当前位置出现滞涨，迟迟不突破，亦要小心。
+          </div>
+        ) : (
+          <div>
+            正在砸出趋势底部，开始密切关注机会，无论是缩量还是放量，都可以开始逐步建仓。
+            <br />
+            寻找跌的最惨的股和近期连板股，关注近期热点题材和新闻，市场情绪极度恐慌时，需要他们带头建立信心。
+            <br />
+            破位下跌过程，可能会有持续，关注第二日是否出现止跌信号，进行操作。
+          </div>
+        );
+      return word;
+    }
+
+    if (numPer >= 70) {
+      word =
+        score > 0 ? (
+          <div>
+            当前正在开始走向趋势顶部，开始注意风险，密切关注成交量，发现当日成交量减少后，可开始逐渐减仓。
+            <br />
+            同时结合趋势分析结果，综合判断，当从上升趋势转为下跌趋势时，轻仓操作。
+            <br />
+            见顶过程中，一般会出现天量。或是在当前位置出现滞涨，迟迟不突破，亦要小心。
+          </div>
+        ) : (
+          <div>
+            正在砸出趋势底部，开始密切关注机会，无论是缩量还是放量，都可以开始逐步建仓。
+            <br />
+            寻找跌的最惨的股和近期连板股，关注近期热点题材和新闻，市场情绪极度恐慌时，需要他们带头建立信心。
+            <br />
+            破位下跌过程，可能会有持续，关注第二日是否出现止跌信号，进行操作。
+          </div>
+        );
+      return word;
+    }
+  };
+
   return (
-    <section className="relative">
+    <section className="relative hero-home-wrap">
       <PageIllustration />
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
         {/* Hero content */}
@@ -75,23 +125,58 @@ export default function HeroHome() {
             data-aos="zoom-y-out"
             data-aos-delay={600}
           >
-            <div>
-              连续
-              {indexData.consecutive_up_days
-                ? `上涨${indexData.consecutive_up_days}天`
-                : `下跌${indexData.consecutive_down_days}天`}
-            </div>
-            <div>
-              当前为{indexData['当前趋势']}
-            </div>
-            {
-              indexData['是否剧烈振幅'] ? <div>今日剧烈震荡 {indexData['是否加速']}</div> : null
-            }
-            <div>
-              {indexData['是否反转'] ? '反转成立' : '暂未反转'}
-            </div>
-            <div>
-              {indexData['最新涨跌幅']}
+            <div className="index-module">
+              <h2 className="module-title">上证指数</h2>
+              <div className="module-content">
+                当前连续
+                <span
+                  style={{
+                    color: indexData.consecutive_up_days
+                      ? "#f46649"
+                      : "#2aa491",
+                  }}
+                >
+                  {indexData.consecutive_up_days > 0
+                    ? ` 上涨${indexData.consecutive_up_days}天`
+                    : ` 下跌${indexData.consecutive_down_days}天`}
+                </span>
+              </div>
+              <div className="module-content">
+                当前趋势为
+                <span
+                  style={{
+                    color:
+                      indexData["当前趋势"] === "上行趋势"
+                        ? "#f46649"
+                        : "#2aa491",
+                  }}
+                >
+                  {` ${indexData["当前趋势"]}`}
+                </span>
+              </div>
+              {indexData["是否剧烈振幅"] ? (
+                <div className="module-content">
+                  今日剧烈震荡 {indexData["是否加速"]}
+                </div>
+              ) : null}
+              <div className="module-content">
+                当前
+                <span
+                  style={{
+                    color:
+                      indexData["反转数据"].score > 0 ? "#f46649" : "#2aa491",
+                  }}
+                >
+                  {` ${indexData["反转数据"].score > 0 ? "见顶" : "见底"} `}
+                </span>
+                的概率为
+                <span className="text-[#f46649] text-500">
+                  {" "}
+                  {indexData["反转数据"].percent}%{" "}
+                </span>
+                {renderTrendWord(100, -20)}
+              </div>
+              <div className="module-content">{indexData["最新涨跌幅"]}</div>
             </div>
           </div>
         </div>
