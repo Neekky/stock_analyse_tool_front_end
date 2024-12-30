b73b0c100a91a115fb64b92b458eb22e83db366f
cengchao <475383200@qq.com>
2024-12-30 21:39:45 +0800

    1.增加新功能

--- src/pages/stock_combat_cockpit/components/winners-volume-detail/index.tsx
+++ src/pages/stock_combat_cockpit/components/winners-volume-detail/index.tsx
@@ -166,8 +166,7 @@ const StockChart = ({ data, indexKline }) => {
       <div ref={chartRef} style={{ width: "100%", height: "700px" }} />
       <div className="w-full market-tip-wrap">
         <div className="text-[18px] font-semibold	text-[#b91c1c]">使用小贴士</div>
-        <div className="text-[16px] text-[#78716c] mt-2">1. 红线为沪深两市总成交额，蓝线为60日平均线</div>
-        <div className="text-[16px] text-[#78716c] mt-2">2. 观察两线的金叉、死叉情况</div>
+        <div className="text-[16px] text-[#78716c] mt-2">1. 观察各路资金变化情况</div>
       </div>
     </>
   );
